9867e0a633be03a7c6405bcca6f9ad607a2fffca
jaeho.jung <jaeho.jung@worksmobile.com>
2018-08-24 17:42:50 +0900

    todo initail

---
+++ src/components/todo/TodoDetailContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoDetail extends Component {
+    render() {
+        return (
+            <div>
+                Todo Detail Container
+            </div>
+        );
+    }
+}
+
+export default TodoDetail;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import TodoDetailContainer from '../src/components/todo/TodoDetailContainer';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -17,3 +18,8 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+  storiesOf('TodoDetail', module)
+    .add('Todo Detail Container', () => (
+      <TodoDetailContainer />
+    ));
